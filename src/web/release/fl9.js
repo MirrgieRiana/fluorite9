909d8ae0a6f0cb29a762c0852417341146a69a11
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-07-01 04:58:42 +0900

    [console] [add] ノード・JSコードを入出力する機能

--- src/web/release/fl9.js
+++ src/web/release/fl9.js
@@ -2,8 +2,71 @@ const parser = require("./fl9_parser.js");
 const compiler = require("./fl9_compiler.js");
 const runtime = require("./fl9_runtime.js");
 
-const src = process.argv[2];
-const node = parser.parse(src);
-const code = compiler.fl9.getStandardCompiler()(node);
-const result = runtime.toString(eval(code)(runtime));
-console.log(result);
+function usage() {
+  console.log("Usage: [-N] [-n] [-C] [-c] [--] <exec>");
+  process.exit(1);
+}
+
+let nodeInput = false;
+let nodeOutput = false;
+let codeInput = false;
+let codeOutput = false;
+let exec = undefined;
+
+const argv = [...process.argv];
+argv.shift();
+argv.shift();
+while (argv.length > 0) {
+
+  if (argv[0] === "-N") {
+    nodeInput = true;
+    argv.shift();
+    continue;
+  }
+  if (argv[0] === "-n") {
+    nodeOutput = true;
+    argv.shift();
+    continue;
+  }
+  if (argv[0] === "-C") {
+    codeInput = true;
+    argv.shift();
+    continue;
+  }
+  if (argv[0] === "-c") {
+    codeOutput = true;
+    argv.shift();
+    continue;
+  }
+  if (argv[0] === "--") {
+    argv.shift();
+    if (argv.length === 0) usage();
+    exec = argv[0];
+    argv.shift();
+    continue;
+  }
+  exec = argv[0];
+  argv.shift();
+  continue;
+}
+
+if (exec === undefined) usage();
+
+//
+
+function main() {
+  const src = exec;
+  const node = codeInput ? src : nodeInput ? JSON.parse(src) : parser.parse(src);
+  if (nodeOutput) {
+    console.log(JSON.stringify(node, undefined, "  "));
+    return;
+  }
+  const code = codeInput ? node : compiler.fl9.getStandardCompiler()(node);
+  if (codeOutput) {
+    console.log(code);
+    return;
+  }
+  const result = runtime.toString(eval(code)(runtime));
+  console.log(result);
+}
+main();
