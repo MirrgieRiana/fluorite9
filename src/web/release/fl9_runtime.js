3a971957132ae9972434f4314fff0836f55d784a
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-07-05 02:41:21 +0900

    [refactor] fl9_runtime

--- src/web/release/fl9_runtime.js
+++ src/web/release/fl9_runtime.js
@@ -8,160 +8,184 @@
   }
 }(this, function(_) {
   "use strict";
-  function Runtime() {
-    this._libraries = [];
-  }
-  const runtime = Runtime.prototype;
-  runtime.symbolToString = Symbol("fl9ToString");
-  runtime.symbolAdd = Symbol("fl9Add");
-  runtime.symbolSubtract = Symbol("fl9Subtract");
-  runtime.symbolMultiply = Symbol("fl9Multiply");
-  runtime.symbolDivide = Symbol("fl9Divide");
-  runtime.symbolStream = Symbol("fl9Stream");
 
-  runtime.Fl9Stream = class Fl9Stream {
-    constructor(func) {
-      this[runtime.symbolStream] = func;
+  const symbolToString = Symbol("fl9ToString");
+  const symbolAdd = Symbol("fl9Add");
+  const symbolSubtract = Symbol("fl9Subtract");
+  const symbolMultiply = Symbol("fl9Multiply");
+  const symbolDivide = Symbol("fl9Divide");
+  const symbolStream = Symbol("fl9Stream");
+
+  class Fl9Stream {
+    constructor(runtime, func) {
+      this._runtime = runtime;
+      this[symbolStream] = func;
     }
-    [runtime.symbolToString]() {
+    [symbolToString]() {
       let string = "";
       let first = true;
-      for (let item of this[runtime.symbolStream]()) {
+      for (let item of this[symbolStream]()) {
         if (first) {
           first = false;
         } else {
           string += "\n";
         }
-        string += runtime.toString(item);
+        string += this._runtime.toString(item);
       }
       return string;
     }
-  };
+  }
 
-  runtime.empty = new runtime.Fl9Stream(function*() { });
-  runtime.void = {
-    [runtime.symbolToString]() {
-      throw new Error("Void access");
-    },
-    [runtime.symbolStream]() {
-      throw new Error("Void access");
+  class Runtime {
+    constructor {
+      this._libraries = [];
     }
-  };
-  runtime.addLibrary = function(library) {
-    this._libraries.unshift(library);
-  };
-  runtime.get = function(name) {
-    for (let library of this._libraries) {
-      const value = library[name];
-      if (value !== undefined) return value;
+
+    addLibrary(library) {
+      this._libraries.unshift(library);
     }
-    throw Error(`Unknown Variable: ${name}`)
-  };
-  runtime.set = function(name, value) {
-    throw Error(`Unknown Variable: ${name}`)
-  };
-  runtime.toNumber = function(value) {
-    if (typeof value === "number") return value;
-    if (typeof value === "string") return parseFloat(value);
-    if (typeof value === "boolean") return value ? 1 : 0;
-    throw new Error("Illegal Action: toNumber(" + value + ")");
-  };
-  runtime.toString = function(value) {
-    if (typeof value === "string") return value;
-    if (typeof value === "number") return "" + value;
-    if (typeof value === "boolean") return value ? "TRUE" : "FALSE";
-    if (value === undefined) return "UNDEFINED";
-    if (value === null) return "NULL";
-    if (value instanceof Array) return value.map(item => runtime.toString(item)).join(",");
-    if (typeof value === "object" && value[runtime.symbolToString] !== undefined) return value[runtime.symbolToString]();
-    if (typeof value === "object") return Object.getOwnPropertyNames(value).map(name => `${name}:${runtime.toString(value[name])};`).join("");
-    throw new Error("Illegal Action: toString(" + value + ")");
-  };
-  runtime.toBoolean = function(value) {
-    if (typeof value === "boolean") return value;
-    if (typeof value === "number") return value !== 0;
-    if (typeof value === "string") return value !== "";
-    throw new Error("Illegal Action: toBoolean(" + value + ")");
-  };
-  runtime.toStream = function(value) {
-    if (value[runtime.symbolStream] !== undefined) return value;
-    return new runtime.Fl9Stream(function*() {
-      yield value;
-    });
-  };
-  runtime.getLength = function(value) {
-    if (typeof value === "string") return value.length;
-    if (value instanceof Array) return value.length;
-    throw new Error("Illegal Action: getLength(" + value + ")");
-  };
-  runtime.add = function(left, right) {
-    if (typeof left === "number") return left + runtime.toNumber(right);
-    if (typeof left === "string") return left + runtime.toString(right);
-    if (left instanceof Array) {
-      if (!(right instanceof Array)) throw new Error(`Illegal Argument: ${left.constructor.name} + ${right.constructor.name}`);
-      return [...left, ...right];
+
+    getEmpty() {
+      return new this.Fl9Stream(this, function*() { });
     }
-    if (typeof left === "object" && left[runtime.symbolAdd] !== undefined) return left[runtime.symbolAdd](left, right);
-    throw new Error(`Illegal Argument: ${left.constructor.name}.add(${right.constructor.name})`);
-  };
-  runtime.subtract = function(left, right) {
-    if (typeof left === "number") return left - runtime.toNumber(right);
-    if (typeof left === "object" && left[runtime.symbolSubtract] !== undefined) return left[runtime.symbolSubtract](left, right);
-    throw new Error(`Illegal Argument: ${left.constructor.name}.subtract(${right.constructor.name})`);
-  };
-  runtime.multiply = function(left, right) {
-    if (typeof left === "number") return left * runtime.toNumber(right);
-    if (typeof left === "string") return left.repeat(runtime.toNumber(right));
-    if (left instanceof Array) return Array.from({length: runtime.toNumber(right)}).flatMap(item => left);
-    if (typeof left === "object" && left[runtime.symbolMultiply] !== undefined) return left[runtime.symbolMultiply](left, right);
-    throw new Error(`Illegal Argument: ${left.constructor.name}.multiply(${right.constructor.name})`);
-  };
-  runtime.divide = function(left, right) {
-    if (typeof left === "number") return left / runtime.toNumber(right);
-    if (typeof left === "object" && left[runtime.symbolDivide] !== undefined) return left[runtime.symbolDivide](left, right);
-    throw new Error(`Illegal Argument: ${left.constructor.name}.divide(${right.constructor.name})`);
-  };
-  runtime.apply = function(value, args) {
-    if (value instanceof Function) {
-      return value.apply(null, args);
-    } else if (value instanceof Array) {
-      return value[args[0]];
-    } else {
-      throw new Error(`Illegal Argument: ${value.constructor.name}[${args.constructor.name}]`);
+    getVoid() {
+      return {
+        [symbolToString]: () => throw new Error("Void access"),
+        [symbolAdd]: () => throw new Error("Void access"),
+        [symbolSubtract]: () => throw new Error("Void access"),
+        [symbolMultiply]: () => throw new Error("Void access"),
+        [symbolDivide]: () => throw new Error("Void access"),
+        [symbolStream]: () => throw new Error("Void access"),
+      };
     }
-  };
-  runtime.createDelegate = function(object, key) {
-    const symbol = Symbol("<DELEGATE>")
-    return {[symbol]: function() {
-      return object[key].apply(object, [object, ...arguments]);
-    }}[symbol];
-  };
-  runtime.rangeOpened = function(start, endExcluded) {
-    start = runtime.toNumber(start);
-    endExcluded = runtime.toNumber(endExcluded);
-    return new runtime.Fl9Stream(function*() {
-      for (let i = start; i < endExcluded; i++) {
-        yield i;
+
+    get(name) {
+      for (let library of this._libraries) {
+        const value = library[name];
+        if (value !== undefined) return value;
       }
-    });
-  };
-  runtime.rangeClosed = function(start, endExcluded) {
-    start = runtime.toNumber(start);
-    endExcluded = runtime.toNumber(endExcluded);
-    return new runtime.Fl9Stream(function*() {
-      for (let i = start; i <= endExcluded; i++) {
-        yield i;
+      throw Error(`Unknown Variable: ${name}`)
+    }
+    set(name, value) {
+      throw Error(`Unknown Variable: ${name}`)
+    }
+
+    toStream(value) {
+      if (value[symbolStream] !== undefined) return value;
+      return new this.Fl9Stream(this, function*() {
+        yield value;
+      });
+    }
+
+    toNumber(value) {
+      if (typeof value === "number") return value;
+      if (typeof value === "string") return parseFloat(value);
+      if (typeof value === "boolean") return value ? 1 : 0;
+      throw new Error("Illegal Action: toNumber(" + value + ")");
+    }
+    toString(value) {
+      if (typeof value === "string") return value;
+      if (typeof value === "number") return "" + value;
+      if (typeof value === "boolean") return value ? "TRUE" : "FALSE";
+      if (value === undefined) return "UNDEFINED";
+      if (value === null) return "NULL";
+      if (value instanceof Array) return value.map(item => this.toString(item)).join(",");
+      if (typeof value === "object" && value[symbolToString] !== undefined) return value[symbolToString]();
+      if (typeof value === "object") return Object.getOwnPropertyNames(value).map(name => `${name}:${this.toString(value[name])};`).join("");
+      throw new Error("Illegal Action: toString(" + value + ")");
+    }
+    toBoolean(value) {
+      if (typeof value === "boolean") return value;
+      if (typeof value === "number") return value !== 0;
+      if (typeof value === "string") return value !== "";
+      throw new Error("Illegal Action: toBoolean(" + value + ")");
+    }
+
+    getLength(value) {
+      if (typeof value === "string") return value.length;
+      if (value instanceof Array) return value.length;
+      throw new Error("Illegal Action: getLength(" + value + ")");
+    }
+
+    add(left, right) {
+      if (typeof left === "number") return left + this.toNumber(right);
+      if (typeof left === "string") return left + this.toString(right);
+      if (left instanceof Array) {
+        if (!(right instanceof Array)) throw new Error(`Illegal Argument: ${left.constructor.name} + ${right.constructor.name}`);
+        return [...left, ...right];
       }
-    });
-  };
-  runtime.map = function(stream, func) {
-    stream = runtime.toStream(stream);
-    return new runtime.Fl9Stream(function*() {
-      for (item of stream[runtime.symbolStream]()) {
-        yield func(item);
+      if (typeof left === "object" && left[symbolAdd] !== undefined) return left[symbolAdd](left, right);
+      throw new Error(`Illegal Argument: ${left.constructor.name}.add(${right.constructor.name})`);
+    }
+    subtract(left, right) {
+      if (typeof left === "number") return left - this.toNumber(right);
+      if (typeof left === "object" && left[symbolSubtract] !== undefined) return left[symbolSubtract](left, right);
+      throw new Error(`Illegal Argument: ${left.constructor.name}.subtract(${right.constructor.name})`);
+    }
+    multiply(left, right) {
+      if (typeof left === "number") return left * this.toNumber(right);
+      if (typeof left === "string") return left.repeat(this.toNumber(right));
+      if (left instanceof Array) return Array.from({length: this.toNumber(right)}).flatMap(item => left);
+      if (typeof left === "object" && left[symbolMultiply] !== undefined) return left[symbolMultiply](left, right);
+      throw new Error(`Illegal Argument: ${left.constructor.name}.multiply(${right.constructor.name})`);
+    }
+    divide(left, right) {
+      if (typeof left === "number") return left / this.toNumber(right);
+      if (typeof left === "object" && left[symbolDivide] !== undefined) return left[symbolDivide](left, right);
+      throw new Error(`Illegal Argument: ${left.constructor.name}.divide(${right.constructor.name})`);
+    }
+
+    apply(value, args) {
+      if (value instanceof Function) {
+        return value.apply(null, args);
+      } else if (value instanceof Array) {
+        return value[args[0]];
+      } else {
+        throw new Error(`Illegal Argument: ${value.constructor.name}[${args.constructor.name}]`);
       }
-    });
-  };
+    }
+    createDelegate(object, key) {
+      const symbol = Symbol("<DELEGATE>")
+      return {[symbol]: function() {
+        return object[key].apply(object, [object, ...arguments]);
+      }}[symbol];
+    }
+    rangeOpened(start, endExcluded) {
+      start = this.toNumber(start);
+      endExcluded = this.toNumber(endExcluded);
+      return new this.Fl9Stream(this, function*() {
+        for (let i = start; i < endExcluded; i++) {
+          yield i;
+        }
+      });
+    }
+    rangeClosed(start, endExcluded) {
+      start = this.toNumber(start);
+      endExcluded = this.toNumber(endExcluded);
+      return new this.Fl9Stream(this, function*() {
+        for (let i = start; i <= endExcluded; i++) {
+          yield i;
+        }
+      });
+    }
+    map(stream, func) {
+      stream = this.toStream(stream);
+      return new this.Fl9Stream(this, function*() {
+        for (item of stream[symbolStream]()) {
+          yield func(item);
+        }
+      });
+    }
+
+  }
+  Runtime.prototype.symbolToString = symbolToString;
+  Runtime.prototype.symbolAdd = symbolAdd;
+  Runtime.prototype.symbolSubtract = symbolSubtract;
+  Runtime.prototype.symbolMultiply = symbolMultiply;
+  Runtime.prototype.symbolDivide = symbolDivide;
+  Runtime.prototype.symbolStream = symbolStream;
+  Runtime.prototype.Fl9Stream = Fl9Stream;
+
   _.Runtime = Runtime;
   return _;
 }));
