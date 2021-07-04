e3cb8abdec226d42d8161cce785c9b48e72f9b1f
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-07-04 22:08:56 +0900

    [test]

--- src/test/sh/test.sh
+++ src/test/sh/test.sh
@@ -15,5 +15,9 @@
 # 構文木出力モード・JSコード出力モードを経由しても結果は変わらない
 [ 15 == "$(  fl9 '1 + 2 + 3 + 4 + 5' -n | fl9 -f - -N -c | fl9 -f - -C  )" ] || exit
 
+# JSコード出力したものをnodeで実行できる
+fl9 '1 + 2 + 3 + 4 + 5' -c > tmp1.js
+[ 15 == "$(  node -e 'console.log(require("./tmp1.js").main(require("fl9_runtime.js")))'  )" ] || exit
+
 echo "test.sh OK"
 exit 0
--- test.sh
+++ test.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 dir=$(cd "$(dirname "$0")"; pwd)
-cd "$dir"/build/web/release || exit
+
+mkdir -p "$dir"/build/test || exit
+cd "$dir"/build/test || exit
 
 # language
-NODE_PATH=$dir/build/web/release node "$dir"/src/test/js/test.js || exit
+NODE_PATH="$dir"/build/web/release node "$dir"/src/test/js/test.js || exit
 
 # console interface
 chmod +x "$dir"/build/web/release/fl9
-PATH=$PATH:$dir/build/web/release bash "$dir"/src/test/sh/test.sh || exit
+PATH=$PATH:"$dir"/build/web/release NODE_PATH="$dir"/build/web/release bash "$dir"/src/test/sh/test.sh || exit
