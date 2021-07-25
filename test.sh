7e6b0dd2e50bdde9a8c4b0bd5095e2ba2eb142d4
Mirrgie Riana <tacticsrealize2@gmail.com>
2021-07-25 13:07:15 +0900

    [test] [internal] PATH

--- test.sh
+++ test.sh
@@ -4,10 +4,15 @@ dir=$(cd "$(dirname "$0")"; pwd)
 mkdir -p "$dir"/build/test || exit
 cd "$dir"/build/test || exit
 
+PATH=$dir/build/web/release:$PATH
+export PATH
+NODE_PATH="$dir"/build/web/release
+export NODE_PATH
+
 # language
-NODE_PATH="$dir"/build/web/release node "$dir"/src/test/js/test.js || exit
+node "$dir"/src/test/js/test.js || exit
 
 # console interface
 chmod +x "$dir"/build/web/release/fl9
 chmod +x "$dir"/build/web/release/fl9e
-PATH="$dir"/build/web/release:$PATH NODE_PATH="$dir"/build/web/release bash "$dir"/src/test/sh/test.sh || exit
+bash "$dir"/src/test/sh/test.sh || exit
